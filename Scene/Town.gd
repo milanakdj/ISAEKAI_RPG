bd7adb3a47039f84853a098862b2810613addc6c
milanakdj <milanakdj@gmail.com>
2021-09-17 18:41:38 +0545

    fixed the in game main menu

--- Scene/Town.gd
+++ Scene/Town.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
 onready var menu = $Menu/Control
-
+onready var top_menu = $Menu
 
 func _ready():
 	menu.visible = false
+	
 	if Global.spawn_position:
 		$YSort/Player.global_position = Global.spawn_position
-
+	
+	$Menu.connect("CloseMenu", self, "_on_Exit")	
+	
+	
 func _input(event):
-	if event.is_action_pressed("open_menu"):
+	if event.is_action_pressed("open_menu") and $Menu/Control.visible == false:
 		$Menu/Control.visible = true
 		$Menu/Control/backMusic.playing = true
-		
-
-func _on_Exit_pressed():
+		#$Menu._unhandled_input(event)
+	elif event.is_action_pressed("open_menu") and $Menu/Control.visible == true:
+		pass
+func _on_Exit():
 	menu.visible = false
 	$Menu/Control/backMusic.playing = false
--- mainmenu/Menu.gd
+++ mainmenu/Menu.gd
@@ -10,7 +10,7 @@ onready var selectedarrow = $Control/TextureRect
 onready var music = $Control/backMusic
 
 enum ScreenLoaded { NOTHING, JUST_MENU, TUTORIAL}
-var screenLoaded = ScreenLoaded.NOTHING
+export var screenLoaded = ScreenLoaded.NOTHING
 
 var selected_option: int = 0
 
@@ -22,26 +22,24 @@ func _unhandled_input(event):
 	match screenLoaded:
 		ScreenLoaded.NOTHING:
 			if event.is_action_pressed("open_menu"):
-				
 				var player = get_tree().get_root().get_node(currentScene).find_node("YSort").find_node("Player")
 				player.set_physics_process(false)
 				get_tree().get_root().get_node(currentScene).find_node("MainCamera").set_process_input(false)
-					
 				menu.visible = true 
 				screenLoaded = ScreenLoaded.JUST_MENU
 				
 		ScreenLoaded.JUST_MENU:
 			if event.is_action_pressed("open_menu") or event.is_action_pressed("x"):
-				_on_Exit_pressed()
+				emit_signal("CloseMenu")
 				screenLoaded = ScreenLoaded.NOTHING
+				
 				var player = get_tree().get_root().get_node(currentScene).find_node("YSort").find_node("Player")
 				player.set_physics_process(true)
 				get_tree().get_root().get_node(currentScene).find_node("MainCamera").set_process_input(true)
 			
 			elif event.is_action_pressed("ui_down") or event.is_action_pressed("down"):
 				selected_option +=1
-				selectedarrow.rect_position.y = 64 + (selected_option % 4) * 20
-				 
+				selectedarrow.rect_position.y = 64 + (selected_option % 4) * 20 
 				
 			elif event.is_action_pressed("ui_up") or event.is_action_pressed("up"):
 				if selected_option == 0:
@@ -49,16 +47,26 @@ func _unhandled_input(event):
 				else :
 					selected_option -=1
 					selectedarrow.rect_position.y = 64 + (selected_option % 4) * 20
-				
+			
+			elif event.is_action_pressed("ui_accept"):
+				match selected_option:
+					0:
+						pass
+					1:
+						pass
+					2:
+						_on_Tutorial_pressed()
+					3:
+						emit_signal("CloseMenu")
+						
 		ScreenLoaded.TUTORIAL:
 			if event.is_action_pressed("open_menu") or event.is_action_pressed("x"):
 				menu.visible = false
 				screenLoaded = ScreenLoaded.NOTHING
 				
 
-func _on_Exit_pressed():
+func _on_Exit():
 	emit_signal("CloseMenu")
-
 
 func _on_Tutorial_pressed():
 	var tutorial_menu = load("res://mainmenu/TutorialMenu.tscn").instance()
@@ -67,3 +75,7 @@ func _on_Tutorial_pressed():
 	
 func CloseTutorialMenus():
 	get_node("Control/TutorialMenu").queue_free()
+
+
+func _on_Exit_pressed():
+	emit_signal("CloseMenu")
