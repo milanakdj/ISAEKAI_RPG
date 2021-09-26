0af25af075ff7f5b464d0bc942aabef212711fb1
milanakdj <milanakdj@gmail.com>
2021-09-26 18:01:21 +0545

    added day night cycle

---
+++ Scene/DayNightCycle.gd
@@ -0,0 +1,11 @@
+extends CanvasModulate
+
+const Night_color  = Color("#415c77")
+const Day_color = Color("#ffffff")
+const Time_scale = 0.1
+
+var time = 0 
+
+func _process(delta):
+	self.time  += delta * Time_scale
+	self.color = Night_color.linear_interpolate(Day_color, abs(sin(time))	)
