0fa81f19ca40fa54dd330aeb63ad95b5ed38cd5d
milanakdj <milanakdj@gmail.com>
2021-10-09 18:20:11 +0545

    added the bats to appears as soon as the quest starts

--- Quest_UI.gd
+++ Quest_UI.gd
@@ -9,7 +9,20 @@ var objective_counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var rand = RandomNumberGenerator.new()
+	var enemyscene = load("res://Mobs/Bat.tscn")	
+	
+	var screen_size = get_viewport().get_visible_rect().size
+	
+	for i in range(0, 10):
+		var enemy = enemyscene.instance()
+		rand.randomize()
+		var x = rand.randf_range(0, screen_size.x)
+		rand.randomize()
+		var y= rand.randf_range(0, screen_size.y)
+		enemy.position.y=y
+		enemy.position.x=x
+		get_tree().get_root().get_node("Town").find_node("YSort").add_child(enemy)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
