988594924e268732aa9033fd58a7271d11a0c964
milanakdj <milanakdj@gmail.com>
2021-10-22 20:00:47 +0545

    fixed the scrollbar, vboxcontainer, buttons for the savemenu

--- mainmenu/SavingMenu.gd
+++ mainmenu/SavingMenu.gd
@@ -6,24 +6,28 @@ signal CloseGoalsMenu
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$NinePatchRect/ScrollContainer/VBoxContainer/Button1/Label.text = "whatsss up"
-	var i=1
-	var button ="$NinePatchRect/ScrollContainer/VBoxContainer/Button"+String(i)+"/Label"
+	var i=0
+	var my_dict= PoolStringArray()
+	var current_line = {}
+	#var button ="$NinePatchRect/ScrollContainer/VBoxContainer/Button"+String(i)+"/Label"
 	var save_game = File.new()
 	if not save_game.file_exists("user://savegame.save"):
 		return 
 	save_game.open("user://savegame.save", File.READ)
 	while not save_game.eof_reached():
-		var current_line = parse_json(save_game.get_line())# returns a dictionary
+		current_line = parse_json(save_game.get_line())# returns a dictionary
 		if current_line != null:
-			for property in current_line.keys():
-				for buttons in $NinePatchRect/ScrollContainer/VBoxContainer.get_children():
-					buttons.get_child("Label").text = "ff"
-				#button.text = current_line["filename"]
-				i=i+1
-				#button ="$NinePatchRect/ScrollContainer/VBoxContainer/Button"+String(i)+"/Label"
+			my_dict.push_back(current_line["filename"])
+			i=i+1
+	i=i-1
+	for buttons in $NinePatchRect/ScrollContainer/VBoxContainer.get_children():
+		if i != -1:
+			buttons.get_child(0).text = my_dict[i]
+			print(my_dict[i])
+			i=i-1
 	save_game.close()
 		
-		
+	
 	
 
 
